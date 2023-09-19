e74777b02d6f350850f987b729f130aaab5679f9
Vincent Tamet <vincent.tamet@exoclick.com>
2023-09-19 23:55:13 +0200

    Change verbose

--- README.md
+++ README.md
@@ -7,17 +7,22 @@ Extract informations from Atlassian.
 # Usage
 ```
 ./atlassian-cli.py --help
-./atlassian-cli.py --help
 options:
   -h, --help       show this help message and exit
   --client CLIENT  Choose the credential (default: exo)
   --users          List users (default: False)
   --debug          Debug information (default: False)
-  --noverbose      Verbose (default: False)
+  --verbose        Verbose (default: False)
 
 ./atlassian-cli.py
+╭────┬─────────────────────────────────────────────┬─────────────┬──────────────┬──────────┬─────────┬──────────────────┬──────────╮
+│    │ accountId                                   │ accountType │ displayName  │ active   │ locale  │ emailAddress     │ timeZone │
+├────┼─────────────────────────────────────────────┼─────────────┼──────────────┼──────────┼─────────┼──────────────────┼──────────┤
+│  0 │ 000000:ffffffff-ffff-ffff-ffff-ffffffffffff │ atlassian   │ User1 User01 │ False    │ es_ES   │ email@domain.ext │          │
+
 ```
 
 
 # History
+/!\ nextPageToken not supported yet !!
 Still in quick & dirty dev phase!
--- atlassian-cli.py
+++ atlassian-cli.py
@@ -11,6 +11,7 @@ import requests
 import json
 import argparse
 import sys, os.path
+from http.client import HTTPConnection  # Debug mode
 from configparser import ConfigParser
 from requests.auth import HTTPBasicAuth
 from tabulate import tabulate
@@ -18,35 +19,103 @@ from tabulate import tabulate
 api_ver = '3'
 api_url = 'https://exogroup.atlassian.net/rest/api/'+api_ver+'/'
 
-def request( resource, param1='', param2='',  param3='', method='GET', headers={"accept": "application/json",} ):
+def request( method='GET', resource='' , param='', headers={} ):
   url=api_url+resource
-  if not param1:
-    url_f=url
-  else:
-    url_f=url+'?'+param1
-  if param2:
-    url_f=url_f+'&'+param2
-  if param3:
-    url_f=url_f+'&'+param3
-  if (args.debug):
-    print(url_f)
-  response = requests.request(
+  headers.update({'accept': 'application/json'})
+  if (args.verbose) or (args.debug):
+    print(url)
+    if (args.debug):
+      # print statements from `http.client.HTTPConnection` to console/stdout
+      HTTPConnection.debuglevel=1
+  response=requests.request(
     method,
-    url_f,
+    api_url+resource,
     headers=headers,
     auth=auth
   )
-  if not (args.noverbose) or (args.debug):
+  if (args.verbose) or (args.debug):
+    print('Status code: '+str(response.status_code))
+  if (args.debug):
     print(json.dumps(json.loads(response.text), sort_keys=True, indent=2, separators=(',', ': ')))
   return(response.json())
 
+def istype( table ):
+  # print('type:'+istype(['1','2']))
+  # print('type:'+istype({'1':'1','2':'2'}))
+  if isinstance(table, dict):    # French: Dictionnaire (Table de hachage)
+    return('dict')
+  elif isinstance(table, float): #         Nombre à virgule flottante
+    return('float')
+  elif isinstance(table, int):   #         Nombre entier
+    return('int')
+  elif isinstance(table, list):  #         Liste (Tableau)
+    return('list')
+  elif isinstance(table, str):   #         Chaîne de caractères
+    return('str')
+  elif isinstance(table, tuple): #         Multiplet
+    return('tuple')
+  else:
+    return('?')
+
+def print_tabulate( table, tablefmt='rounded_outline', stralign='left', showindex=True, sort=False, reverse=False, sortcolumn='', headers='keys', tablefilterkeys=[], tablefilter=[]):
+  # Quick and dirty function!
+  # tablefmt: 'simple', 'rounded_outline', 'simple_outline', 'github', ...
+  # stralign: 'left', 'center', 'right'
+  # missingval='?'
+  # showindex: True, False, "Iterable"
+  # headers: [array], 'firstrow', 'keys' (if dict)
+  # disable_numparse=True
+  if (args.verbose) or (args.debug):
+    print('type:'+istype(table))
+  if tablefilterkeys:
+    if (args.verbose) or (args.debug):
+      print('Tablesfilterkeys:'+str(tablefilterkeys))
+    newtable= []
+    for key in table:
+      if (args.debug):
+        print('Key:'+str(key))
+      for tablefilterkey in tablefilterkeys:
+         key.pop(tablefilterkey,None)
+         newtable.append(key)
+    tables=newtable
+  if tablefilter:
+    if (args.verbose) or (args.debug):
+      print('Filter'+str(tablefilter))
+    if (args.debug):
+      print(table)
+    if isinstance(table, list):
+      table=np.delete(table, tablefilter, axis=1)
+    if isinstance(table, dict):
+      print('!!!')
+      table=table.drop('photoUrl', inplace=True, axis=1)
+  if sort: # Didn't find another way for sort! :(
+    if not sortcolumn:
+      sortcolumn=next(iter(table[0]))
+    if (args.verbose) or (args.debug):
+      print('Sort:'+sortcolumn)
+      print('Reverse:'+str(reverse))
+    #sortkey=(lambda item: (item['displayName']))
+    sortkey=(lambda item: (item[sortcolumn]))
+    if (args.verbose) or (args.debug):
+      print('Sortkey:'+str(sortkey))
+    if args.noheaders:
+      print(tabulate(sorted(table, reverse=reverse, key=sortkey), tablefmt='plain', showindex=showindex, stralign=stralign))
+    else:
+      print(tabulate(sorted(table, reverse=reverse, key=sortkey), tablefmt=tablefmt, headers=headers, showindex=showindex, stralign=stralign))
+  else:
+    if args.noheaders:
+      print(tabulate(table, tablefmt='plain', showindex=showindex, stralign=stralign))
+    else:
+      print(tabulate(table, tablefmt=tablefmt, headers=headers, showindex=showindex, stralign=stralign))
 #############################################################################
 parser = argparse.ArgumentParser(description='https://github.com/osgpcq/atlassian-cli-py',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--client',               default='exo',       help='Choose the credential')
 parser.add_argument('--users',                action='store_true', help='List users')
+parser.add_argument('--noheaders',            action='store_true', help='No headers in the output')
 parser.add_argument('--debug',                action='store_true', help='Debug information')
-parser.add_argument('--noverbose',            action='store_true', default=False, help='Verbose')
+#parser.add_argument('--verbose',              action='store_true', default=True, help='Verbose')
+parser.add_argument('--verbose',              action='store_true', default=False, help='Verbose')
 args = parser.parse_args()
 
 config_file='./config.conf'
@@ -58,6 +127,8 @@ else:
   sys.exit('Configuration file not found!')
 
 users=request( resource='users/search' )
+# /!\ Need to support nextPageToken !!!
+print_tabulate( table=users, tablefilterkeys=['avatarUrls','self'], sort=True, sortcolumn='displayName' )
 #############################################################################
 #############################################################################
 #############################################################################
